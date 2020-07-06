a1cc7cf8941b69479b1f825abf3ed2a3e918450f
Aman Ahuja <ahuja.aman006@gmail.com>
2020-07-06 15:55:02 +0530

    [ADD] theme_inventive

---
+++ inventive_theme_support/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Webkul Software Pvt. Ltd. (<https://webkul.com/>)
+# Copyright(c): 2015-Present Webkul Software Pvt. Ltd.
+# License URL : https://store.webkul.com/license.html/
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+# If not, see <https://store.webkul.com/license.html/>
+#################################################################################
+{
+  "name"                 :  "Theme Inventive Support",
+  "summary"              :  "Containes The Code for Theme Inventive Support",
+  "category"             :  "website",
+  "sequence"             :  1,
+  "author"               :  "Webkul Software Pvt. Ltd.",
+  "license"              :  "Other proprietary",
+  "description"          :  """Containes The Code for Theme Inventive Support""",
+  "depends"              :  [],
+  "data"                 :  [
+                              'views/theme_templates.xml',
+                            ],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+}
